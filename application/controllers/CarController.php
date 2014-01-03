9a16b37e638bd2da129175c26cf0e491cad43ee6
cody92 <codydoc92@gmail.com>
2014-01-03 19:16:05 +0200

    add comments for CarController class

--- application/controllers/CarController.php
+++ application/controllers/CarController.php
@@ -3,6 +3,10 @@
 class CarController extends BdController
 {
 
+    /**
+     *
+     * @var array variabila pentru campurile care trebuiesc verificate in formularul de adaugare/editare masina
+     */
     private $addValidateFields = array(
         'name' => 'Nume marca',
         'description' => 'Descriere marca',
@@ -15,15 +19,7 @@ class CarController extends BdController
 
     public function index()
     {
-        $dashboardItems = array(
-            array(
-                'css-icon' => 'icon-user',
-                'head-title' => 'Add new car',
-                'tooltip-text' => 'Add a new car',
-                'link' => ''
-            ),
-        );
-        $this->set('items', $dashboardItems);
+        $this->redirect('dasboard/index');
     }
 
     public function afterAction()
@@ -31,15 +27,25 @@ class CarController extends BdController
 
     }
 
+    /**
+     * formular adaugare marca noua
+     */
     public function adaugaMarca()
     {
-        //logo upload is not implemented yet
+        /*
+         * upload logo nu este implementat
+         * daca este trimisa cererea verificam datele trimise
+         */
+
         if (isset($_POST['add'])) {
             $result = $this->validateData($_POST, $this->addValidateFields);
+
             if (count($result)) {
+                //daca sunt erori reincarcam formularul cu erori afisate si datele trimise in cerere
                 $this->set('errors', $result);
                 $this->set('data', $_POST);
             } else {
+                //adaugam o inregistrare noua in tabela auto
                 $sql = "INSERT INTO auto (`name`, `description`) VALUES (?, ?)";
                 $stmt = $this->db->prepare($sql);
                 $stmt->execute(array($_POST['name'], $_POST['description']));
@@ -51,18 +57,29 @@ class CarController extends BdController
         }
     }
 
+    /**
+     * formular editare marca, primeste ca parametru idul marcii care trebuie editat
+     *
+     * @param int $id
+     * @return int
+     */
     public function edit($id = null)
     {
+        //daca nu este primit ca parametru idul marcii, redirect la pagina de home
         if (!$id && !is_numeric($id)) {
             $this->redirect('dashboard/index');
             return 0;
         }
+
         if (isset($_POST['add'])) {
+            //validam datele trimise prin POST
             $result = $this->validateData($_POST, $this->addValidateFields);
             if (count($result)) {
+                //daca sunt erori reincarcam formularul cu erori afisate si datele trimise in cerere
                 $this->set('errors', $result);
                 $this->set('data', $_POST);
             } else {
+                // facem update la inregistrare curenta
                 $sql = "UPDATE auto SET name = ?, description = ? WHERE id = ?";
                 $stmt = $this->db->prepare($sql);
                 $stmt->execute(
@@ -77,6 +94,7 @@ class CarController extends BdController
                 }
             }
         } else {
+            //incarcam datele despre marca in formular
             $sql = "SELECT * FROM auto WHERE id = ?";
             $stmt = $this->db->prepare($sql);
             $stmt->execute(array($id));
@@ -86,6 +104,14 @@ class CarController extends BdController
         }
     }
 
+    /**
+     * metoda pentru verificarea datelor inainte de ediatare si adaugare, verificare doar pentru campurile care sunt
+     * obligatorii
+     *
+     * @param array $data datele care trebuiesc validate primite prin POST
+     * @param array $columns coloanele care trebuiesc validate
+     * @return array returneaza un vector multidimensional cu erori
+     */
     private function validateData($data, $columns)
     {
         $errors = array();
@@ -106,6 +132,9 @@ class CarController extends BdController
         return $errors;
     }
 
+    /**
+     * listarea tuturor marcilor din baza de date, tabela auto
+     */
     public function autoList()
     {
         $sql = "SELECT a.*, (SELECT COUNT(m.id) FROM model as m WHERE m.autoId = a.id) AS totalModels, "
